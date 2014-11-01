7bfc961e7932e3a1e2927278b02e1da56af1b3a3
Greg Fedirko <gjfedirko@gmail.com>
2014-11-01 14:18:43 +0200

    functioning three axis rotation

--- server.js
+++ server.js
@@ -1,5 +1,19 @@
+var express = require('express');
 var serialPortModule = require('serialport');
-var SerialPort = serialPortModule.SerialPort
+var SerialPort = serialPortModule.SerialPort;
+
+var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+io.on('connection', function(socket){
+  console.log('Socket.io connected');
+});
+
+
+app.get('/', function(req, res) {
+  res.sendfile('index.html');
+});
 
 var sp = new SerialPort("/dev/tty.usbmodem1421", {
   baudrate: 115200,
@@ -9,8 +23,15 @@ var sp = new SerialPort("/dev/tty.usbmodem1421", {
 sp.on("open", function() {
   console.log("serialport opened");
 
+
   sp.on("data", function(data) {
-    console.log(data);
+    io.emit('data', {data: data});
   });
 
 });
+
+
+http.listen(3000, function(){
+  console.log('listening on *:3000');
+});
+
